44c1aac3153fe9f0ed361420c72249ff4ecab8ad
KZacharski <puffercatt@gmail.com>
2022-07-18 21:17:17 +0200

    Version 1.0.2 - final pufferflat update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "app.lawnchair.lawnicons"
         minSdk = 26
         targetSdk = 31
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2 - final pufferflat update"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
